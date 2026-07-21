16a12454c17f49b58bbb8225567968b3ad74ec4c
Abhishek Pal <150524042+ABHI002684@users.noreply.github.com>
2026-07-21 20:19:13 +0530

    Time: 83 ms (42.79%), Space: 80.8 MB (37.39%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        int n=s.size();
+
+        //count existing one's
+        int activeCount=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='1'){
+                activeCount++;
+            }
+        }
+
+        vector<int> inactiveBlocks;
+        int i=0;
+        while(i<n){
+            if(s[i]=='0'){
+                int start=i;
+                while(i<n && s[i]=='0') i++;
+
+                inactiveBlocks.push_back(i-start);
+            }else{
+                i++;
+            }
+        }
+
+        int maxPairSum=0;
+        for(int i=1;i<inactiveBlocks.size();i++){
+            maxPairSum=max(maxPairSum,inactiveBlocks[i]+inactiveBlocks[i-1]);
+        }
+
+        return maxPairSum+activeCount;
+    }
+};
