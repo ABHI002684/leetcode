29c726025c305fc5249a8a4807a933285aac22b1
Abhishek Pal <150524042+ABHI002684@users.noreply.github.com>
2026-08-16 21:30:58 +0530

    Time: 3 ms (75.38%), Space: 131.2 MB (92.89%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int a = 0, b = 0, c = 0;
+
+        for (int stone : stones) {
+            if (stone % 3 == 0) {
+                a++;
+            } else if (stone % 3 == 1) {
+                b++;
+            } else {
+                c++;
+            }
+        }
+
+        if (a % 2 == 0) {
+            return b > 0 && c > 0;
+        }
+
+        return abs(b - c) > 2;
+    }
+};
